a1c74a9c075f0c362091c947793cbb3f82bf3d12
Nate Morgan <nathanielrmorgan@gmail.com>
2023-01-02 12:07:43 -0500

    stub up basic card outline in the list

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,9 @@
+const Card = (props) => {
+  return ( 
+    <div class='card'>
+      <p> This is some sample text for the cards </p>
+    </div>
+  );
+}
+
+export default Card;
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,9 +1,14 @@
+import Card from "../Card/Card";
+
 const List = (props) => {
   return ( 
     <div class='list'>
-      <h2> {props.listName} </h2>
+      <header class='list-header'>
+        <h3 style={{margin:'0'}}> {props.listName} </h3>
+        <button style={{height:'40px',width:'40px',textAlign:'center', fontSize:'large',fontWeight:'bold'}}> + </button>
+      </header>
+      <Card/>
     </div>
-
   );
 }
 
